d2f65abe9495e3d09abbbf7e0ea5a3f57b87236a
Outtacosmos-ai <messghir4@gmail.com>
2024-09-05 15:56:06 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+""" queries the Reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """ returns the number of subscribers for a given subreddit. """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json().get('data')
+        subscribers = data.get('subscribers')
         return subscribers
-    else:
+    except (KeyError, requests.RequestException):
         return 0
