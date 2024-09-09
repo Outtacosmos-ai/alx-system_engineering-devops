3a4fd2460f80dfb0fc23061149087b825c71d962
Outtacosmos-ai <messghir4@gmail.com>
2024-09-09 17:38:20 +0100

    different ways

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
 """
-file of number of subscribers method
+Module for querying the Reddit API and returning the number of subscribers
+for a given subreddit.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    number of subscribers method
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the subreddit is valid, 0 otherwise.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
+        return data["data"]["subscribers"]
     else:
         return 0
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print(number_of_subscribers(sys.argv[1]))
