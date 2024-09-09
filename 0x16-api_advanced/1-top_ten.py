1b428116f9134f8f2871793f395e2b359e0ef741
Outtacosmos-ai <messghir4@gmail.com>
2024-09-09 18:30:21 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+
 """
-file of number of subscribers method
+query the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
-import requests
+
+from requests import get
+from sys import argv
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit: str) -> None:
     """
-    number of subscribers method
+    function that does the heavy lifting for us
+    Args:
+        subreddit (str) -> The subreddit to query
+    Returns: The top 10 hots for that subreddit
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        "User-Agent": "Marvel's Agents of Shield/21",
+        "X-Forwared-For": "Phil J. Coulson"
+    }
+
+    request_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
     try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
+        response = get(request_url, headers=headers,
+                       allow_redirects=False).json()
+        data = response['data']['children']
+        [print(post['data']['title']) for post in data[:10]]
     except Exception:
         print("None")
+
+
+if __name__ == "__main__":
+    (top_ten(argv[1]))
